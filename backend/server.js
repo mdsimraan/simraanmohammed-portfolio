998307cdc2fbb1710905f6d6ceb1578b702c2fe8
AnandYellapu <anandsaiii1200@gmail.com>
2025-07-31 18:21:04 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'], 
+  origin: ['https://anand-yellapu-portfolio.netlify.app/', 'https://anand-yellapu-portfolio.netlify.app/'], 
   credentials: true
 }));
 app.use(express.json());
